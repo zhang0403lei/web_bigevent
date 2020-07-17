9142456c34f2ffce2f2813cd3cb6c7a28dd809b2
zhanglei <783461582@qq.com>
2020-07-17 20:10:04 +0800

    修改login.html

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,7 @@ $(function () {
 
     // 从layui获取form
     var form = layui.form
+    console.log(form)
     var layer = layui.layer
 
     form.verify({
@@ -58,7 +59,6 @@ $(function () {
                     return layer.msg('登录失败')
                 }
                 layer.msg('登录成功')
-
                 localStorage.setItem('token', res.token)
                 location.href = '/index.html'
             }
