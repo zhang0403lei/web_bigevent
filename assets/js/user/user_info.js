8407631de4bee59f2c147eb2560cfa0e95ab8539
zhanglei <783461582@qq.com>
2020-07-20 14:11:01 +0800

    密码以及头像页面完成

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -51,3 +51,28 @@ $(function() {
   })
 
 
+// $(function () {
+//     var form = layui.form
+//     form.verify ({
+//         nickname: function(value) {
+//             if(value.length > 6) {
+//                 return '昵称长度必须是1~6之间'
+//             }
+//         }
+//     })
+//     initUserInfo()
+
+//     function initUserInfo(){
+//         $.ajax({
+//             metnod:'GET',
+//             url:'/my/userinfo',
+//             success: function(res){
+//                 if(res.status !==0) {
+//                     return layer.msg('获取用户信息失败！')
+//                 }
+//                 console.log(res);
+//                 form.val('formUserInfo', res.data)
+//             }
+//         })
+//     }
+// })
